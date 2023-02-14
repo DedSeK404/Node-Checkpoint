e95d489a3a660936e383288bf6f72f79ef9b2dd7
DedSeK404 <marwenbennaceur5@gmail.com>
2023-02-14 11:37:56 +0100

    third commit

--- serverNonExpress.js
+++ serverNonExpress.js
@@ -1,5 +1,6 @@
 const http = require("http")
 const server = http.createServer((req, res)=>{
-    console.log("hello server")
+    res.write("<h1 style='color:red'>Hello Node!!!!</h1>")
+    res.end()
 })
-server.listen(3000, (err)=> err? console.log(err) : console.log('Hello Node!!!!'))
+server.listen(3000, (err)=> err? console.log(err) : console.log('listening on port 3000'))
